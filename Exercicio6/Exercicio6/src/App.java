136f9ea1487d2bb298e8d85317cbd653ecbf60ee
Mateus Barcelos <mateuspit@hotmail.com>
2024-03-06 15:54:36 -0300

    refactor: ex6

--- Exercicio6/Exercicio6/src/App.java
+++ Exercicio6/Exercicio6/src/App.java
@@ -1,66 +1,63 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
-import java.util.List;
-import java.io.Console;
+import java.util.Scanner;
 
 public class App {
 
     private static final String[] holidaysList = { "01-01-2024", "Confraternização Mundial", "12-02-2024", "Carnaval",
-            "13-02-2024", "Carnaval", "29-03-2024", "Sexta-feira Santa", "21-04-2024", "Tiradentes", "01-05-2024",
-            "Dia do Trabalho", "30-05-2024", "Corpus Christi", "07-09-2024", "Independência do Brasil",
+            "13-02-2024", "Carnaval", "29-03-2024", "Sexta-feira Santa", "21-04-2024", "Tiradentes",
+            "01-05-2024", "Dia do Trabalho", "30-05-2024", "Corpus Christi", "07-09-2024", "Independência do Brasil",
             "12-10-2024", "Nossa Senhora Aparecida", "02-11-2024", "Finados", "15-11-2024",
             "Proclamação da República", "20-11-2024", "Dia Nacional de Zumbi e da Consciência Negra", "25-12-2024",
             "Natal" };
 
-    public static String askDate() {
-        Console console = System.console();
-        String questionDate = "Qual data quer analisar se é feriado? ";
+    private static final Scanner scanner = new Scanner(System.in);
 
-        return parseInput(console.readLine(questionDate));
-    }
+    public static LocalDate askDate() {
+        String questionDate = "Qual data quer verificar se é feriado? (Formato: dd-MM-yyyy) ";
+        LocalDate date = null;
+        boolean entradaValida = false;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
-    public static String parseInput(String date) {
-        int dateSize = date.length();
-        boolean rightSeparator = date.contains("-");
-
-        if (dateSize != 10 || !rightSeparator) {
-            System.out.println(
-                    "Números que representam dia e mês devem ter dois digitos e ano quatro digitos separados por -");
-            System.out.println("Ex: 01-01-2024");
-            askDate();
-
+        while (!entradaValida) {
+            try {
+                String entradaUsuario = scanner.nextLine();
+                date = LocalDate.parse(entradaUsuario, formatter);
+                entradaValida = true;
+            } catch (DateTimeParseException e) {
+                System.out.println("Formato de data inválido. Por favor, tente novamente.");
+                System.out.print(questionDate);
+            }
         }
         return date;
     }
 
     public static void printHolidays() {
-        for (int i = 0; i < holidaysList.length; i++) {
-            if (i % 2 == 0) {
-                System.out.print(holidaysList[i] + " => ");
-            } else {
-                System.out.println(holidaysList[i]);
-            }
+        for (int i = 0; i < holidaysList.length; i += 2) {
+            System.out.println(holidaysList[i] + " => " + holidaysList[i + 1]);
         }
     }
 
-    public static void isHoliday(String date) {
-        List<String> holidayList = Arrays.asList(holidaysList);
-        int indexInHolidayList = -1;
-        if (!date.contains("2024")) {
-            String dateWithoutYear = date.substring(0, 5);
-            String dateWith2024 = dateWithoutYear + "-2024";
-            indexInHolidayList = holidayList.indexOf(dateWith2024);
-        } else {
-            indexInHolidayList = holidayList.indexOf("12-10");
-        }
+    public static void isHoliday(LocalDate date) {
+        String dataString = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        int indexNaListaDeFeriados = Arrays.asList(holidaysList).indexOf(dataString);
 
-        if (indexInHolidayList == -1) {
-            System.out.println("Dia " + date + " não é feriado 🥲");
+        if (indexNaListaDeFeriados == -1) {
+            System.out.println("O dia " + dataString + " não é feriado 🥲");
         } else {
-            System.out.println("Dia " + date + " é " + holidayList.get(indexInHolidayList + 1) + "! 🎉");
+            System.out.println("O dia " + dataString + " é " + holidaysList[indexNaListaDeFeriados + 1] + "! 🎉");
         }
     }
 
     public static void main(String[] args) {
-        isHoliday(askDate());
+        System.out.println("Lista de Feriados Disponíveis:");
+        printHolidays();
+        System.out.println();
+
+        System.out.print("Por favor, insira a data para verificar se é feriado: ");
+        LocalDate dataInput = askDate();
+        isHoliday(dataInput);
     }
 }
